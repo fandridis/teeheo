0b5ba13d0ca7a051c3d6c3ac2336a61370581c74
Georgios Fandridis <fandridis@gmail.com>
2019-04-11 09:11:12 +0200

    Split methods to files

--- index.js
+++ index.js
@@ -4,18 +4,30 @@
 	(global = global || self, global.teeheo = factory());
 }(this, function () { 'use strict';
 
+	/**
+	 * 
+	 * @param {integer} min The minimum number to be included in the randomizer
+	 * @param {*} max The maximum number to be included in the randomizer
+	 * @param {*} options Optional - TBA
+	 */
+	function randomInteger (min, max, options) {
+		if (!min) { return 0; }
+
+		if (min && typeof min !== 'number') { throw new TypeError('First argument has to be a number'); }
+		if (max && typeof max !== 'number') { throw new TypeError('Second argument has to be a number'); }
+		
+		if (!max) {
+			max = min;
+			min = 0;
+		}
+
+		return Math.floor(Math.random()*(max-min+1)+min);
+	}
+
 	// import ms from 'ms';
-	// import lunchtime from './lunchtime.js';
-	// import millisecondsUntil from './millisecondsUntil.js';
 
 	const teeheo = {
-		int: (min, max) => {
-			if (typeof min !== 'number' || typeof max !== 'number') {
-				throw new TypeError('Missing arguments');
-			}
-		
-			return Math.floor(Math.random()*(max-min+1)+min);
-		},
+		int: function(min, max) { return randomInteger(min, max); },
 
 		str: (chars) => {
 			return Math.random().toString(36).substring(2, chars + 2).toUpperCase()
